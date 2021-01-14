708842321d89cd294a04290ab72626fa7f911b46
Allie <alliecaton3@gmail.com>
2021-01-14 15:14:18 -0500

    commit tree bd7ab264c8d5bf3a289e5a4fa34449e0d2600ad5

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,28 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+    
+    def initialize(id:, name:, type:, db: nil)
+        @id = id 
+        @name = name 
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        sql= <<-SQL
+            INSERT INTO pokemon (name, type) 
+            VALUES (?, ?);
+        SQL
+        db.execute(sql, name, type)
+        id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+            SELECT * FROM pokemon WHERE id = ?;
+        SQL
+        array = db.execute(sql, id)[0]
+        Pokemon.new(id: array[0], name: array[1], type: array[2])
+    end
 end
